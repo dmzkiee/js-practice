c402fee216194b04a7dbd336d86b414bbbb18d47
cristine jhed infante <cristinejhedinfante@gmail.com>
2025-03-27 20:39:44 +0800

    Initial commit with app.js

---
+++ variables.js
@@ -0,0 +1,17 @@
+// Declare a variable 'name' and store your name
+let name = "Dan Mark Habon";
+
+// Declare a variable for career goal
+let whatDoYouWannaBecomeInYourLife = "Programmer";
+
+// Declare a variable for gender
+let gender = "Male"; // Change it accordingly
+
+// Declare a variable for Twitter handle
+let twitterHandle = "danmarkhabon@twitterHandle";
+
+// Print all variables to the console
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
